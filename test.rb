0a478fb8198544bd188a37e90cc7444545aba210
Fangfei Chen <fangfei1star@gmail.com>
2019-10-27 01:28:57 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,45 @@
+
+# count = 0 # A bit of data defined outside the loop
+# while count < 3 do # A Boolean expression using the bit of data
+#   puts "I am the #{count}, I love to count!" # Work
+#   count = count + 1 # A bit of work that moves the bit of data closer to being false
+# end
+
+# count = 0
+# n = 3
+# loop do
+#   break if count >= n**2
+#   puts "I ran."
+#   count += 1
+# end
+
+# def greetings_this_user (name)
+#   puts "Hello, #{name}"
+# end
+
+# greetings_this_user("Kyle")
+
+# def greeting_programmer (name, language)
+#   puts("Hello, #{name}! We heard you are a great #{language} programmer!")
+# end
+# greeting_programmer("Kyle", "Python")
+
+# def a_method (a,b)
+#   puts "hi"
+#   a+b
+# end
+# re_trun = a_method(1,3)
+# puts re_trun
+
+
+
+h = Hash[[["1", a],["2",b]]]
+
+
+
+
+
+
+
+
+
